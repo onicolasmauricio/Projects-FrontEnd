8470cbb06ebd3ff1c3c133dc49eed21d4d812e7b
Nicolas Maurício <ni.almeida@icloud.com>
2025-10-02 19:51:05 -0300

    Tag link inserida na página Home

--- primefilm/src/pages/Home/index.js
+++ primefilm/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link} from 'react-router-dom';
 
 // URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=5f5ee116a732f1dabd8b46a9e2be297e&language=pt-BR
 
@@ -29,6 +30,8 @@ function Home(){
                 return(
                     <article key={filme.id}>
                         <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
                     </article>
                 )
             })}
