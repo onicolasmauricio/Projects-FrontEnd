257cc6743deb6b2fad38db64527307fd3d4769fa
Nicolas Maurício <ni.almeida@icloud.com>
2025-11-10 20:15:32 -0300

    Rota de Erro criada/inserida

--- primefilm/src/routes.js
+++ primefilm/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+import Erro from './pages/Erro';
+
 import Header from "./components/Header";
 
 function RouterApp(){
@@ -12,6 +14,8 @@ function RouterApp(){
         <Routes>
             <Route path="/" element={<Home/>} />
                 <Route path="/filme/:id" element={ <Filme/>} />
+                
+                <Route path="*" element={<Erro/>} />
         </Routes>
         </BrowserRouter>
     )
