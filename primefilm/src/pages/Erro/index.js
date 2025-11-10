a45e2b56576c2c0dfbcbc21563a5b1a9519c7118
Nicolas Maurício <ni.almeida@icloud.com>
2025-11-10 20:29:21 -0300

    Página de erro finaalizada (formatação)

--- primefilm/src/pages/Erro/index.js
+++ primefilm/src/pages/Erro/index.js
@@ -3,7 +3,7 @@ import "./erro.css";
 
 function Erro(){
     return(
-        <div>
+        <div className="not-found">
             <h1>404</h1>
             <h2>Página não encontrada!</h2>
             <Link to="/">Veja todos filmes!</Link>
