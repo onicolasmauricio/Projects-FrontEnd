bb3a5f21b87afe9e56fa89d0caec83c69ca44110
Nicolas Maurício <ni.almeida@icloud.com>
2025-11-10 20:19:11 -0300

    Código script adicionado/modificado

--- primefilm/src/pages/Erro/index.js
+++ primefilm/src/pages/Erro/index.js
@@ -3,7 +3,11 @@ import "./erro.css";
 
 function Erro(){
     return(
+<<<<<<< HEAD
         <div className="not-found">
+=======
+        <div>
+>>>>>>> 2a7633e (Cósido script adicionado/modificado)
             <h1>404</h1>
             <h2>Página não encontrada!</h2>
             <Link to="/">Veja todos filmes!</Link>
