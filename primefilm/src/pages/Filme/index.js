ed65bc391d0a904b16992396bd718eb9cb2fa257
Nicolas Maurício <ni.almeida@icloud.com>
2026-01-27 18:12:03 -0300

    Creating and showing details from movies

--- primefilm/src/pages/Filme/index.js
+++ primefilm/src/pages/Filme/index.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from '../../services/api';
+
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){ 
+            await api.get(`/movie/${id}`, {
+                params:{ 
+                api_key: "5f5ee116a732f1dabd8b46a9e2be297e",
+                language: "pt-BR",  
+                }
+            })
+            .then((response)=>{ 
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não encontrado!")
+            })
+        }
+
+        loadFilme();
+        
+        return() => {
+            console.log("Componente desmontado")
+        }
+    }, [])
+
+    if(loading){
+        return( 
+        <div className="filme-info">
+            <h1>Carregando detalhes do filme...</h1>
+        </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem-vindo a página de Filme</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+        <h3>Sinopse</h3>
+        <span>{filme.overview}</span>
+
+        <strong>Avaliação: {filme.vote_average} /10</strong>
         </div>
     )
 }
