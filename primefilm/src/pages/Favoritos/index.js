3b313db5784711d34c6292d7c5b894522eb154c4
Nicolas Maurício <ni.almeida@icloud.com>
2026-02-03 21:50:31 -0300

    File's favorite updated (showing informations at screen)

--- primefilm/src/pages/Favoritos/index.js
+++ primefilm/src/pages/Favoritos/index.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
 import './favoritos.css'
+import { Link } from 'react-router-dom'
 
 function Favoritos(){
+
+const [movies, setMovies] = useState([])
+
+useEffect(()=>{
+    const myList = localStorage.getItem("@primeflix");
+    setMovies(JSON.parse(myList) || [])
+
+}, [])
+
 return(
-    <div>
-        <h1>Tela de favoritos</h1>
+    <div className='my-movies'>
+        <h1>Meus filmes</h1>
+
+        <ul>
+            {movies.map((item) => { 
+                return(
+                    <li key={item.id}>
+                        <span>{item.title}</span>
+                        <div>
+                            <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                            <button>Excluir</button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
     </div>
 )
 }
